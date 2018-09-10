cfa684e862008721d7dcd8f76aa97be5d9019c0e
wanglintao <wanglintao@1fangxin.cn>
2018-09-10 14:33:39 +0800

    添加clockwork

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Bugsnag\BugsnagLaravel\BugsnagLaravelServiceProvider::class,
+        Folklore\GraphQL\ServiceProvider::class,
+        Clockwork\Support\Laravel\ClockworkServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class
 
     ],
@@ -227,6 +229,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'GraphQL' => Folklore\GraphQL\Support\Facades\GraphQL::class,
+        'Clockwork' => Clockwork\Support\Laravel\Facade::class,
 
     ],
 
--- routes/web.php
+++ routes/web.php
@@ -11,20 +11,24 @@
 |
 */
 
+use App\Models\User;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/t', function () {
-    ding()->at(["15517515025"],true)
-        ->text("我就是我,@15517515025 是不一样的烟火");
-    $title = '杭州天气';
-    $markdown = "#### 杭州天气  \n ".
-        "> 9度，@1825718XXXX 西北风1级，空气良89，相对温度73%\n\n ".
-        "> ![screenshot](http://i01.lw.aliimg.com/media/lALPBbCc1ZhJGIvNAkzNBLA_1200_588.png)\n".
-        "> ###### 10点20分发布 [天气](http://www.thinkpage.cn/) ";
-
-    ding()->markdown($title,$markdown);
+    User::find(1);
+    //clock()->startEvent('event_name', 'LaravelAcademy.org');
+    //ding()->at(["15517515025"],true)
+    //    ->text("我就是我,@15517515025 是不一样的烟火");
+    //$title = '杭州天气';
+    //$markdown = "#### 杭州天气  \n ".
+    //    "> 9度，@1825718XXXX 西北风1级，空气良89，相对温度73%\n\n ".
+    //    "> ![screenshot](http://i01.lw.aliimg.com/media/lALPBbCc1ZhJGIvNAkzNBLA_1200_588.png)\n".
+    //    "> ###### 10点20分发布 [天气](http://www.thinkpage.cn/) ";
+    //
+    //ding()->markdown($title,$markdown);
 });
 
 Auth::routes();
